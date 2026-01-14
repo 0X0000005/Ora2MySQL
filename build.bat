ae24b2305697d6d97e9b174e037ab56e48cdd884
null <wzy199498@foxmail.com>
2026-01-14 12:30:23 +0800

    fix: 修复 build.bat 脚本中方括号导致的错误

--- build.bat
+++ build.bat
@@ -65,8 +65,8 @@ if exist o2m-linux (
 
 echo.
 echo 使用方法：
-echo   [命令模式]: o2m.exe -i input.sql -o output.sql
-echo   [Web 模式]:  o2m.exe -web -port 8080
-echo   [帮助信息]:  o2m.exe -h
+echo   命令模式: o2m.exe -i input.sql -o output.sql
+echo   Web 模式:  o2m.exe -web -port 8080
+echo   帮助信息:  o2m.exe -h
 echo.
 echo ========================================
